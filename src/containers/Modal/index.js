c77788a332a87b534b937237bb675bb0fb4ecd5e
bsrinath4839 <srinath@exidio.co>
2024-03-16 23:16:03 +0530

    Added Close Modal when Click outside the Container

--- src/containers/Modal/index.js
+++ src/containers/Modal/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import types from "./modal-types";
 import styles from "./modal.module.scss";
+import { useDispatch } from "react-redux";
+import { CHANGE_MODAL_STATE } from "../../redux/reducers/alerts.reducer";
 
 const Modal = ({ type, ...rest }) => {
   const Component = types[type];
+  const dispatch = useDispatch();
   return (
     <div className={styles.root}>
+      <div
+        className={styles["modal-backdrop"]}
+        onClick={() =>
+          dispatch(CHANGE_MODAL_STATE({ show: false, type: null }))
+        }
+      ></div>
+
       <div className={styles.container}>
         <Component {...rest} />
       </div>
