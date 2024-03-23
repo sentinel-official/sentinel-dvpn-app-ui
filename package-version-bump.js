6330a8205c82a24fd09b3d1ecde7fd95bad43157
bsrinath4839 <srinath@exidio.co>
2024-03-23 11:43:19 +0530

    verion bupm added

---
+++ package-version-bump.js
@@ -0,0 +1,22 @@
+const { exec } = require("child_process");
+const readline = require("readline");
+
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+read.question(
+  "Select a version type: patch | minor | major: ",
+  async (type) => {
+    console.log("type: ", type);
+    if (type && ["patch", "minor", "major"].includes(type)) {
+      await exec(`yarn version --${type}`);
+      await exec(`git add .`);
+      await exec(`git commit -m "chore: package version bumped"`);
+    } else {
+      console.log("You have selected none of the above!");
+    }
+    read.close();
+  }
+);
