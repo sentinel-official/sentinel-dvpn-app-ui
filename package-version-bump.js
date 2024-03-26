c4aa22ed1fd5cfb06a7343b2df0380ef509ac3c5
bsrinath4839 <srinath@exidio.co>
2024-03-26 19:15:27 +0530

    modified bumping

--- package-version-bump.js
+++ package-version-bump.js
@@ -1,21 +1,29 @@
 const { exec } = require("child_process");
-const readline = require("readline");
 
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-read.question(
-  "Select a version type: patch | minor | major: ",
-  async (type) => {
-    if (type && ["patch", "minor", "major"].includes(type)) {
-      await exec(`yarn version --${type}`);
-      await exec(`git add .`);
-      await exec(`git commit -m "chore: package version bumped"`);
-    } else {
-      console.log("You have selected none of the above!");
-    }
-    read.close();
+async function bumpVersion(type) {
+  if (type && ["patch", "minor", "major"].includes(type)) {
+    await exec(`yarn version --${type}`);
+    await exec(`git add .`);
+    await exec(`git commit -m "chore: package version bumped"`);
+  } else {
+    console.log("Invalid version type provided!");
   }
-);
+}
+
+async function run() {
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  readline.question(
+    "Select a version type: patch | minor | major: ",
+    async (type) => {
+      await bumpVersion(type);
+      readline.close();
+    }
+  );
+}
+
+// Call the run function
+run();
