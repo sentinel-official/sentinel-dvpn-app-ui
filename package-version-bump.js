e82efc3a2fc0a73e497cdf95dcbf8ea9411de243
bsrinath4839 <srinath@exidio.co>
2024-03-27 18:13:36 +0530

    v1.0.14

--- package-version-bump.js
+++ package-version-bump.js
@@ -3,10 +3,11 @@ const { exec } = require("child_process");
 async function bumpVersion(type) {
   if (type && ["patch", "minor", "major"].includes(type)) {
     console.log("BUilding...");
-    await exec(`yarn build`);
     await exec(`yarn version --${type}`);
+    await exec(`yarn build`);
     await exec(`git add .`);
     await exec(`git commit -m "chore: package version bumped"`);
+    await exec(`git push`);
   } else {
     console.log("Invalid version type provided!");
   }
